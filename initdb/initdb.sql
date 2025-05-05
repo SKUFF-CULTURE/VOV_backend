8f8394e311ea455bb8f528bc3e4fb218d14ce3c1
GDJ4 <e509elsb@gmail.com>
2025-05-05 17:07:11 +0300

    auth + db + redis works

--- initdb/initdb.sql
+++ initdb/initdb.sql
@@ -2,8 +2,5 @@ CREATE TABLE IF NOT EXISTS public.users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100),
   email VARCHAR(100) UNIQUE,
-  role VARCHAR(50),
-  description TEXT,
-  department_id INTEGER REFERENCES departments(id),
   google_id VARCHAR(255)
 );
